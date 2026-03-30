95f1b24bdeb263297978a0dde2f2cc814cd03dc1
chenkp98 <144636153+chenkp98@users.noreply.github.com>
2026-03-30 23:50:07 +0800

    rename

--- bridgeapt/compute/__init__.py
+++
@@ -1,5 +0,0 @@
-from .extract_coord import extra_six_coord, residue_to_num
-from .compute_dihedrals import compute_dihedrals
-from .pdb2pt import read_pdb
-
-__all__ = ["extra_six_coord", "residue_to_num", "compute_dihedrals", "read_pdb"]
